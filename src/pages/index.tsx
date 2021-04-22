35bd4e02f467a6842e931aae36f3927925620665
HENRIQUE <henrique_holtz@hotmail.com>
2021-04-22 19:59:39 -0300

    Created details of episodes

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,8 +1,12 @@
 import { GetStaticProps } from 'next';
-import { format, parseISO} from 'date-fns'
-import ptBR from 'date-fns/locale/pt-BR'
+import Image from 'next/image';
+import Link from 'next/link'
+import { format, parseISO} from 'date-fns';
+import ptBR from 'date-fns/locale/pt-BR';
 import { api } from '../services/api';
 import { ConvertDurationToTimeString } from '../utils/Date';
+
+import styles from './home.module.scss';
 
 type Episode = {
   id: string;
@@ -10,17 +14,17 @@ type Episode = {
   members: string;
   publishedAt: string;
   thumbnail: string;
-  description: string;
   duration: number;
   durationAsString: string;
   url: string;
 }
 
 type HomeProps = {
-  episodes: Episode[];
+  latestEpisodes: Episode[];
+  allEpisodes: Episode[];
 }
 
-export default function Home(props: HomeProps) {
+export default function Home({ latestEpisodes, allEpisodes }: HomeProps) {
 
   //   SPA (React)
   // useEffect(() => {
@@ -28,10 +32,75 @@ export default function Home(props: HomeProps) {
   // }, [])
 
   return (
-    <>
-      <h1>Index pages</h1>
-      <p>R: {JSON.stringify( props.episodes)}</p>
-    </>
+    <div className={styles.homePage}>
+      <section className={styles.latestEpisodes}>
+        <h2>Last Releases</h2>
+
+        <ul>
+          {latestEpisodes.map(ep => {
+            return (
+              <li key={ep.id}>
+                <Image width={192} height={192} src={ep.thumbnail} alt={ep.title} objectFit="cover"/>
+
+                <div className={styles.episodeDetails}>
+                  <Link href={`/episodes/${ep.id}`}>
+                    <a>{ep.title}</a>
+                  </Link>
+                  <p>{ep.members}</p>
+                  <span>{ep.publishedAt}</span>
+                  <span>{ep.durationAsString}</span>
+                </div>
+
+                <button type="button">
+                  <img src="/play-green.svg" alt="Play episode"/>
+                </button>
+              </li>
+            )
+          })}
+        </ul>
+      </section>
+
+      <section className={styles.allEpisodes}>
+        <h2>All episodes</h2>
+
+        <table cellSpacing={0}>
+          <thead>
+            <tr>
+              <th></th>
+              <th>Podcast</th>
+              <th>Members</th>
+              <th>Date</th>
+              <th>Duration</th>
+              <th></th>
+            </tr>
+          </thead>
+          <tbody>
+            {allEpisodes.map(ep => {
+              return (
+                <tr key={ep.id}>
+                  <td style={{ width: 100 }}>
+                    <Image width={120} height={120} src={ep.thumbnail} alt={ep.title} objectFit="cover"/>
+                  </td>
+                  <td>
+                    <Link href={`/episodes/${ep.id}`}>
+                      <a>{ep.title}</a>
+                    </Link>
+                  </td>
+                  <td>{ep.members}</td>
+                  <td style={{ width: 100 }}>{ep.publishedAt}</td>
+                  <td>{ep.durationAsString}</td>
+                  <td>
+                    <button type="button">
+                      <img src="/play-green.svg" alt="Play episode" />
+                    </button>
+                  </td>
+                </tr>
+              )
+            })}
+          </tbody>
+        </table>
+      </section>
+    </div>
   )
 }
 
@@ -68,14 +137,17 @@ export const getStaticProps: GetStaticProps = async () => {
       }),
       duration: Number(ep.file.duration),
       durationAsString: ConvertDurationToTimeString(Number(ep.file.duration)),
-      description: ep.description,
       url: ep.file.url
     }
   })
+
+  const latestEpisodes = episodes.slice(0,2);
+  const allEpisodes = episodes.slice(2, episodes.length)
   
   return {
     props: {
-      episodes
+      latestEpisodes,
+      allEpisodes
     },
     revalidate: 60 * 60 * 8, //Time in seconds for refresh this page
   }
